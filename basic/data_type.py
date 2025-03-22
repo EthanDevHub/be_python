895ec18700ae56a292731213acfcca6e19e8fb4f
ethan <ethanyenlin@gmail.com>
2025-03-22 16:57:01 +0800

    Begin with the basic knowledge of the python

---
+++ basic/data_type.py
@@ -0,0 +1,34 @@
+
+
+#number
+1024
+
+# String
+"Hello World"
+
+# Bool
+True
+False
+
+# List - sequential, moveable
+[128, 256, 512]
+name_a = ["Alice", "Bob", "Carl"]
+print(name_a[0])
+
+# Tuple - sequential, non-moveable
+(8, 16, 32, 48)
+nato_a = ["Alpha", "Bravo", "Charlie"]
+print(nato_a[1])
+
+
+# Set - non-sequential, but it is O(1) for serach, no same element in one set
+{1024, 2048, 4096}
+fruit_a = {"Apple", "Banana", "Cherry"}
+
+# Dictionary
+{"Alice":"female", "Bob":"male"}
+
+# Var
+x=1024
+print("The x = " + str(x))
+print(f"The x = {x}")
---
+++ basic/number_string.py
@@ -0,0 +1,47 @@
+# integer + float
+val = 7/3
+print(val)
+
+# integer
+val = 7//3
+print(val)
+
+# remainder
+val = 7%3
+print(val)
+
+# times
+val = 2*3
+print(val)
+
+# power
+val = 2**3
+print(val)
+
+# square root
+val = 2**0.5
+print(val)
+
+# add
+val = 10
+val += 1
+print(val)
+
+# strings 
+str_a = "Hello "+"World"
+print(str_a)
+
+str_new_ine = """Hello
+
+World"""
+print(str_new_ine)
+
+str_times = "Hello"*3+"World"
+print(str_times)
+
+# print character
+str_c = "Hello World"
+print(str_c[1])
+print(str_c[1:4])
+print(str_c[1:])
+print(str_c[:4])
